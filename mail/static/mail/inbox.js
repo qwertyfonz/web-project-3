fb433474f72a92664e9bdbbcac646a85fbc57d2f
qwertyfonz <qwertyfonz@DESKTOP-7RAHV6L.localdomain>
2020-07-17 23:53:05 -0400

    Implemented archive, replply, send functionalities

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,77 +5,78 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', () => compose_email());
-
-  // By default, load the inbox
+ 
+  //By default, load the inbox
   load_mailbox('inbox');
 });
 
-function compose_email() {
 
-  console.log("compose_email()");
+function compose_email() {
+  
   // Show compose view and hide other views
-  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#emails-view').style.display  = 'none';
   document.querySelector('#compose-view').style.display = 'block';
-  document.querySelector('#view-email').style.display = 'none';
+  document.querySelector('#view-email').style.display   = 'none';
 
   // Clear out composition fields
-  document.querySelector('#compose-recipients').value = '';
-  document.querySelector('#compose-subject').value = '';
-  document.querySelector('#compose-body').value = '';
+  document.querySelector('#compose-recipients').value   = '';
+  document.querySelector('#compose-subject').value      = '';
+  document.querySelector('#compose-body').value         = '';
+  //document.querySelector('#mailbox').value              ='sent';
 
-  
-  // document.querySelector('form').addEventListener('submit', () => send_email());
-  document.querySelector('form').addEventListener('submit', () => send_email());
+  document.querySelector('#submit-button').addEventListener('click', () => send_email());
+  //document.querySelector('#submit-button').addEventListener('click', () => send_email());
 }
+
 
 function load_mailbox(mailbox) {
-  console.log("load_mailbox="+mailbox);
+  
+  //console.log('mailbox='+ document.querySelector('#mailbox').value);
 
-  // Show the mailbox and hide other views
+  //Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display  = 'block';
   document.querySelector('#compose-view').style.display = 'none';
-  document.querySelector('#view-email').style.display = 'none';
+  document.querySelector('#view-email').style.display   = 'none';
 
-  // Clear any preloaded emails that were previously loaded
+  //Clear any preloaded emails that were previously loaded
   document.querySelector('#view-email').innerHTML = '';
 
-  // Show the mailbox name
+  //Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
+  //Get the contents of an inbox
   get_inbox_content(mailbox);
-
-
-
-
+  
 }
 
-// Fetch contents of an inbox
+
+//Fetch contents of an inbox
 function get_inbox_content(mailbox) {  
   
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
   .then(emails => {
-    // Run create_inbox_view function below 
-    create_inbox_view(emails);
-  });
+    create_inbox_view(mailbox,emails);
+  }) // Run create_inbox_view function below 
+  .catch(error => console.log('Failed while getting the mailbox content',error));
 }
 
 
 
-// Take json response from GET request and display as a table
-function create_inbox_view(emails) {
+//Take json response from GET request and display as a table
+function create_inbox_view(mailbox, emails) {
+  
   let sender;
   let subject;
   let timestamp;
 
-  // Create table
+  //Create table
   const table = document.createElement('table');
-  
+  table.setAttribute('id', 'mailbox_table');
+
   // For every email in the list of emails in the inbox
-  for (var i = 0; i < emails.length; i++) {
-  //emails.forEach(email => {
-    
-    // Get information about the email and store into variables
+  for (var i = 0; i < emails.length; i++) {    
+    //Get information about the email and store into variables
     sender    = emails[i].sender;
     subject   = emails[i].subject;
     timestamp = emails[i].timestamp;
@@ -84,20 +85,16 @@ function create_inbox_view(emails) {
 
     // Create a new entry (table row) for an email
     let tr = document.createElement('tr');
-
-    // Set default color as white
-    tr.style.backgroundColor = "white";
-
-    //tr.addEventListener('click', () => view_email(emailId));
+    tr.setAttribute('id', 'mailbox_tr');//set tr id
+    tr.style.backgroundColor = "white";// Set default color as white
+    
     // Declare a closure to pass a local-scoped index variable (info from: https://softauthor.com/javascript-add-click-event-listener-in-a-loop/#use-foreach-instead-of-for)
     tr.addEventListener('click', function(index) {
       return function() {
-        view_email(emails[index]);
+        view_email(mailbox, emails[index]);
       }
     }(i)) 
    
-      
-    
     // If the email is already read, make its background light grey
     if (isRead) {
       tr.style.backgroundColor = "lightgrey";
@@ -127,61 +124,167 @@ function create_inbox_view(emails) {
   document.querySelector('#emails-view').appendChild(table);
 }
 
-// To view the email
-function view_email(email) {
+
+function print_email (mailbox, emailContainer, email) {
+  
+  //Create table data elements
+  let fromTd      = document.createElement('td');
+  let toTd        = document.createElement('td');
+  let subjectTd   = document.createElement('td');
+  let timestampTd = document.createElement('td');
+  let bodyTd      = document.createElement('td');
+  let buttonsTd   = document.createElement('td');
+  let hr          = document.createElement('hr');
+
+  //Set table data values
+  fromTd.innerHTML      = '<b>From:</b> ' + email.sender;
+  toTd.innerHTML        = '<b>To:</b> ' + email.recipients;
+  subjectTd.innerHTML   = '<b>Subject:</b> ' + email.subject;
+  timestampTd.innerHTML = '<b>Timestamp:</b> ' + email.timestamp;
+  bodyTd.innerHTML      = email.body;
+
+   
+  //Create table rows
+  let fromTr      = document.createElement('tr'); 
+  let toTr        = document.createElement('tr');
+  let subjectTr   = document.createElement('tr'); 
+  let timestampTr = document.createElement('tr'); 
+  let buttonsTr   = document.createElement('tr');
+
+  
+  //Create buttons 
+  let replyButton    = document.createElement('button');
+  replyButton.setAttribute('class','btn btn-primary');
+  replyButton.innerHTML  ='Reply';
+  replyButton.addEventListener('click', () => replyEmail(email));
+  buttonsTd.appendChild(replyButton);
+
+  if (mailbox != 'sent') { 
+    let archiveButton  = document.createElement('button');
+    archiveButton.setAttribute('class', 'btn btn-primary');
+    archiveButton.setAttribute('id', 'archive-button');
+    archiveButton.innerHTML = 'Archive';
+    archiveButton.addEventListener('click', () => updateArchive(email.id, true));
+    //archiveButton.onclick = updateArchive(email.id, true);
+
+    if (email.archived) {
+      archiveButton.innerHTML = 'Unarchive';
+      archiveButton.addEventListener('click', () => updateArchive(email.id, false));
+      //archiveButton.onclick = updateArchive(email.id, false);
+    }
+
+    buttonsTd.appendChild(archiveButton);
+  }
+
+  //archiveButton.removeEventListener('click');
+
+  
+  buttonsTr.appendChild(buttonsTd);
+
+  //Add columns into the rows
+  fromTr.appendChild(fromTd);
+  toTr.appendChild(toTd);  
+  subjectTr.appendChild(subjectTd);
+  timestampTr.appendChild(timestampTd); 
+
+
+  //Create table content
+  let table = document.createElement('table'); 
+  table.setAttribute('id', 'email_header');
+
+  //Add rows into the table
+  table.appendChild(fromTr);
+  table.appendChild(toTr);
+  table.appendChild(subjectTr);
+  table.appendChild(timestampTr);
+  table.appendChild(buttonsTr);
+      
+  //Add table content into html container(div)
+  emailContainer.appendChild(table);
+  emailContainer.appendChild(hr);
+  emailContainer.appendChild(bodyTd);
+   
+}
+
+function replyEmail(email) {
+  document.querySelector('#emails-view').style.display  = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#view-email').style.display   = 'none';
+
+  let emailSubject = email.subject;
+  if (emailSubject.startsWith('Re: ')) {
+    emailSubject = emailSubject.slice(4);
+  }
+
+  document.querySelector('#compose-recipients').value   = `${email.sender}`;
+  document.querySelector('#compose-subject').value      = `Re: ${emailSubject}`;
+  document.querySelector('#compose-body').value         = `On ${email.timestamp} ${email.sender} wrote: \n ${email.body}`;
+
+  document.querySelector('#submit-button').addEventListener('click', () => send_email());
+}
+
+// Mark a given email as read
+function markAsRead (email_id) {  
+  fetch(`/emails/${email_id}`,{
+    method: 'PUT',
+    body: JSON.stringify({read: true})
+  })
+}
+
+// Mark a given email as archived
+function updateArchive (email_id, trueOrFalse) {  
+  /*
+  var jsonObj = {archived: true};
+  if (!trueOrFalse) {
+    jsonObj = {archived: false};
+  }*/
+
+  fetch(`/emails/${email_id}`, {
+    method: 'PUT',
+    body: JSON.stringify({archived: trueOrFalse})
+  })
+  /*.then(response => response.json())
+  .then(result => {
+    console.log('result='+result);
+    load_mailbox('inbox');
+  })
+  .catch(error => {
+    console.log(error);
+  });*/
+
+  document.querySelector('#archive-button').onclick = load_mailbox('inbox'); //window.location.reload();
+  
+}
+
+
+//To view the email
+function view_email(mailbox, email) {  
   
   // Show the email and hide other views
   document.querySelector('#emails-view').style.display  = 'none';
   document.querySelector('#compose-view').style.display = 'none';
-  let emailContainer = document.querySelector('#view-email');
-  emailContainer.style.display = 'block';
+  let emailContainer            = document.querySelector('#view-email');
+  emailContainer.style.display  = 'block';
 
-  // Fetch a specific email based on its id
+  // Fetch a specific email and print it
   fetch(`/emails/${email.id}`)
   .then(response => response.json())
-  .then(email => {
- 
-    // Get information about the email and store into variables
-    let from = document.createElement('p');
-    let to = document.createElement('p');
-    let subject = document.createElement('p');
-    let timestamp = document.createElement('p');
-    let body = document.createElement('p');
+  .then(email => print_email(mailbox, emailContainer, email));
 
-    // Set the inner HTMLs of the above elements 
-    from.innerHTML = '<b>From:</b> ' + email.sender;
-    to.innerHTML = '<b>To:</b> ' + email.recipients;
-    subject.innerHTML = '<b>Subject:</b> ' + email.subject;
-    timestamp.innerHTML = '<b>Timestamp:</b> ' + email.timestamp;
-    body.innerHTML = email.body;
-
-    // Append it to the page
-    emailContainer.appendChild(from);
-    emailContainer.appendChild(to);
-    emailContainer.appendChild(subject);
-    emailContainer.appendChild(timestamp);
-    emailContainer.appendChild(body);
-
-    // Set read status for an email that has been clicked on to true
-    email.read = true;
-
-
-  });
+  //Mark email as read
+  if (!email.read) { 
+    markAsRead(email.id);
+  }
 }
 
-// To compose and send an email
+
+//To compose and send an email
 function send_email() {
-
-  console.log("send_email()");
-
-  // Get recipients, subject, and body from the email
+  
+  //Get recipients, subject, and body from the email
   const recipients  = document.querySelector('#compose-recipients').value;
   const subject     = document.querySelector('#compose-subject').value;
   const body        = document.querySelector('#compose-body').value;
-  
-  console.log("recipients="+recipients);
-  console.log("subject="+subject);
-  console.log("body="+body);
 
   // Post request to /emails route
   fetch('/emails', {
@@ -192,11 +295,11 @@ function send_email() {
       body: body
     })
   })
-  .then(response=> response.json())
-  .then(result=> {
-    console.log(result);
+  .then(response => response.json())
+  .then(result => {
 
     // Load sent mailbox when email is sent
     load_mailbox('sent');
-  });
+  })
+  //.catch(error=>console.warn('Error while sending the email. ', error))
 }
